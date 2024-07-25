e792d40c828160160f94a017791d4b6a444a449f
vs-tech-coder <vaibhavajinathshinde@gmail.com>
2024-07-25 15:20:16 +0530

    Add files via upload

---
+++ ArrayElementAtIndex.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArrayElementAtIndex {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the index to find the element: ");
+        int index = scanner.nextInt();
+        
+        if (index >= 0 && index < array.length) {
+            System.out.println("Element at index " + index + " is: " + array[index]);
+        } else {
+            System.out.println("Invalid index! Please enter a valid index within the range of the array.");
+        }
+        
+        scanner.close();
+    }
+}
